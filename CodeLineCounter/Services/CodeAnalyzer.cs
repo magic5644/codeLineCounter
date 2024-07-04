c5e413ccd869a2f6809845d3b109e69f6e8ab376
Gildas LE BOURNAULT <glebournault@groupeisagri.com>
2024-07-04 14:19:31 +0200

    refactor

--- CodeLineCounter/Services/CodeAnalyzer.cs
+++ CodeLineCounter/Services/CodeAnalyzer.cs
@@ -1,6 +1,9 @@
 using CodeLineCounter.Models;
 using CodeLineCounter.Utils;
 using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CodeLineCounter.Services
 {
@@ -14,106 +17,81 @@ namespace CodeLineCounter.Services
             var namespaceMetrics = new List<NamespaceMetrics>();
             var projectTotals = new Dictionary<string, int>();
             int totalLines = 0;
-            int totalFilesanalyzed = 0;
+            int totalFilesAnalyzed = 0;
 
             foreach (var projectFile in projectFiles)
             {
-                string? projectDirectory = Path.GetDirectoryName(projectFile);
-                string projectName = Path.GetFileNameWithoutExtension(projectFile);
-                string relativeProjectPath = solutionDirectory != null ? Path.GetRelativePath(solutionDirectory, projectFile) : projectFile;
-                
-                var files = projectDirectory != null ? FileUtils.GetAllCsFiles(projectDirectory) : [];
-
-                int projectLineCount = 0;
-                var projectNamespaceMetrics = new Dictionary<string, int>();
-
-                foreach (var file in files)
-                {
-                    totalFilesanalyzed += 1;
-                    projectLineCount = AnalyzeSourceFile(solutionDirectory, namespaceMetrics, projectName, relativeProjectPath, projectLineCount, projectNamespaceMetrics, file);
-
-                }
-
-                // Add subtotals by namespace in the project
-                foreach (var kvp in projectNamespaceMetrics)
-                {
-                    namespaceMetrics.Add(new NamespaceMetrics
-                    {
-                        ProjectName = projectName,
-                        ProjectPath = relativeProjectPath,
-                        NamespaceName = kvp.Key,
-                        FileName = "Total",
-                        FilePath = projectDirectory,
-                        LineCount = kvp.Value,
-                        CyclomaticComplexity = 0  // Subtotals do not require cyclomatic complexity
-                    });
-                }
-
-                // Add project total
-                projectTotals[projectName] = projectLineCount;
-                totalLines += projectLineCount;
+                AnalyzeProject(solutionDirectory, projectFile, ref totalFilesAnalyzed, ref totalLines, namespaceMetrics, projectTotals);
             }
 
-            return (namespaceMetrics, projectTotals, totalLines, totalFilesanalyzed );
+            return (namespaceMetrics, projectTotals, totalLines, totalFilesAnalyzed);
         }
 
-        // Analyzes a source file to obtain metrics such as line count and cyclomatic complexity.
+        private void AnalyzeProject(string solutionDirectory, string projectFile, ref int totalFilesAnalyzed, ref int totalLines, List<NamespaceMetrics> namespaceMetrics, Dictionary<string, int> projectTotals)
+        {
+            string? projectDirectory = Path.GetDirectoryName(projectFile);
+            string projectName = Path.GetFileNameWithoutExtension(projectFile);
+            string relativeProjectPath = solutionDirectory != null ? Path.GetRelativePath(solutionDirectory, projectFile) : projectFile;
+
+            var files = projectDirectory != null ? FileUtils.GetAllCsFiles(projectDirectory) : new List<string>();
+
+            int projectLineCount = 0;
+            var projectNamespaceMetrics = new Dictionary<string, int>();
+
+            foreach (var file in files)
+            {
+                totalFilesAnalyzed++;
+                projectLineCount = AnalyzeSourceFile(solutionDirectory, namespaceMetrics, projectName, relativeProjectPath, projectLineCount, projectNamespaceMetrics, file);
+            }
+
+            AddProjectMetrics(projectName, relativeProjectPath, projectDirectory, projectNamespaceMetrics, namespaceMetrics, projectTotals, projectLineCount);
+            totalLines += projectLineCount;
+        }
+
+        private static void AddProjectMetrics(string projectName, string relativeProjectPath, string? projectDirectory, Dictionary<string, int> projectNamespaceMetrics, List<NamespaceMetrics> namespaceMetrics, Dictionary<string, int> projectTotals, int projectLineCount)
+        {
+            foreach (var kvp in projectNamespaceMetrics)
+            {
+                namespaceMetrics.Add(new NamespaceMetrics
+                {
+                    ProjectName = projectName,
+                    ProjectPath = relativeProjectPath,
+                    NamespaceName = kvp.Key,
+                    FileName = "Total",
+                    FilePath = projectDirectory,
+                    LineCount = kvp.Value,
+                    CyclomaticComplexity = 0
+                });
+            }
+
+            projectTotals[projectName] = projectLineCount;
+        }
+
         public static int AnalyzeSourceFile(string? solutionDirectory, List<NamespaceMetrics> namespaceMetrics, string projectName, string relativeProjectPath, int projectLineCount, Dictionary<string, int> projectNamespaceMetrics, string file)
         {
             var lines = File.ReadAllLines(file);
-            string? currentNamespace;
-            int fileLineCount, fileCyclomaticComplexity;
-            AnalyzeSourceCode(projectNamespaceMetrics, file, lines, out currentNamespace, out fileLineCount, out fileCyclomaticComplexity);
+            AnalyzeSourceCode(projectNamespaceMetrics, file, lines, out string? currentNamespace, out int fileLineCount, out int fileCyclomaticComplexity);
 
-            // Add file metrics
-            if (currentNamespace != null)
+            namespaceMetrics.Add(new NamespaceMetrics
             {
-                namespaceMetrics.Add(new NamespaceMetrics
-                {
-                    ProjectName = projectName,
-                    ProjectPath = relativeProjectPath,
-                    NamespaceName = currentNamespace,
-                    FileName = Path.GetFileName(file),
-                    FilePath = solutionDirectory != null ? Path.GetRelativePath(solutionDirectory, file) : file,
-                    LineCount = fileLineCount,
-                    CyclomaticComplexity = fileCyclomaticComplexity
-                });
-            }
-            else
-            {
-                // File without namespace
-                namespaceMetrics.Add(new NamespaceMetrics
-                {
-                    ProjectName = projectName,
-                    ProjectPath = relativeProjectPath,
-                    NamespaceName = "No Namespace",
-                    FileName = Path.GetFileName(file),
-                    FilePath = solutionDirectory != null ? Path.GetRelativePath(solutionDirectory, file) : file,
-                    LineCount = fileLineCount,
-                    CyclomaticComplexity = fileCyclomaticComplexity
-                });
-            }
-            projectLineCount += fileLineCount;
-            return projectLineCount;
+                ProjectName = projectName,
+                ProjectPath = relativeProjectPath,
+                NamespaceName = currentNamespace ?? "No Namespace",
+                FileName = Path.GetFileName(file),
+                FilePath = solutionDirectory != null ? Path.GetRelativePath(solutionDirectory, file) : file,
+                LineCount = fileLineCount,
+                CyclomaticComplexity = fileCyclomaticComplexity
+            });
+
+            return projectLineCount + fileLineCount;
         }
 
-        /// <summary>
-        /// Analyzes the source code of a file to obtain the cyclomatic complexity and line count for each namespace.
-        /// Updates the provided <paramref name="projectNamespaceMetrics"/> dictionary with the line count for each namespace.
-        /// </summary>
-        /// <param name="projectNamespaceMetrics">The dictionary to store the line count for each namespace.</param>
-        /// <param name="file">The path of the file to analyze.</param>
-        /// <param name="lines">The lines of the file to analyze.</param>
-        /// <param name="currentNamespace">Output parameter indicating the current namespace being analyzed.</param>
-        /// <param name="fileLineCount">Output parameter indicating the total number of lines in the file.</param>
-        /// <param name="fileCyclomaticComplexity">Output parameter indicating the cyclomatic complexity of the file.</param>
         public static void AnalyzeSourceCode(Dictionary<string, int> projectNamespaceMetrics, string file, string[] lines, out string? currentNamespace, out int fileLineCount, out int fileCyclomaticComplexity)
         {
             currentNamespace = null;
             fileLineCount = 0;
             fileCyclomaticComplexity = 0;
 
-            // Analyze the file to obtain cyclomatic complexity
             var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file));
             var compilation = CSharpCompilation.Create("CodeAnalysis", new[] { tree });
             var model = compilation.GetSemanticModel(tree);
