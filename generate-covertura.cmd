06ef379543724ec47e53ea43e18e5d353c1b257b
magic56 <magic56@free.fr>
2024-12-04 12:20:12 +0100

    fix: correct command for removing older test results in generate-covertura.cmd

--- generate-covertura.cmd
+++ generate-covertura.cmd
@@ -1,6 +1,6 @@
  rem echo off
  echo
  rem - remove older test results
-if exist rmdir /s /q CodeLineCounter.Tests\TestResults
+rmdir /s /q CodeLineCounter.Tests\TestResults\
  rem - collect code coverage
 dotnet test --collect:"XPlat Code Coverage;Format=json,lcov,cobertura"  --results-directory CodeLineCounter.Tests\TestResults --logger trx;LogFileName=testresults.trx
