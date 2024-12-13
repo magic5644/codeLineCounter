0bf57b8af872c57e1016d743e6927037110cd218
Gildas Le Bournault <glebournault@gmail.com>
2024-12-13 19:25:33 +0100

    feat: improve dependency analysis with full type name resolution

--- CodeLineCounter/Services/DependencyAnalyzer.cs
+++ CodeLineCounter/Services/DependencyAnalyzer.cs
@@ -16,13 +16,67 @@ namespace CodeLineCounter.Services
     public class DependencyAnalyzer
     {
         private static readonly ConcurrentDictionary<string, HashSet<DependencyRelation>> _dependencyMap = new();
+        private static readonly HashSet<string> _solutionClasses = new();
         private static readonly object _dependencyLock = new();
 
         public static void AnalyzeSolution(string solutionFilePath)
         {
             var projectFiles = FileUtils.GetProjectFiles(solutionFilePath);
+            CollectAllClasses(projectFiles);
 
             AnalyzeProjects(projectFiles);
+        }
+
+        private static void CollectAllClasses(IEnumerable<string> projectFiles)
+        {
+            foreach (var projectFile in projectFiles)
+            {
+                string projectDirectory = Path.GetDirectoryName(projectFile) ?? string.Empty;
+                var files = FileUtils.GetAllCsFiles(projectDirectory);
+
+                foreach (var file in files)
+                {
+                    var sourceCode = File.ReadAllText(file);
+                    var tree = CSharpSyntaxTree.ParseText(sourceCode);
+                    var root = tree.GetRoot();
+
+                    var classes = root.DescendantNodes()
+                        .OfType<ClassDeclarationSyntax>()
+                        .Select(c => GetFullTypeName(c));
+
+                    lock (_dependencyLock)
+                    {
+                        foreach (var className in classes)
+                        {
+                            _solutionClasses.Add(className);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string GetFullTypeName(ClassDeclarationSyntax classDeclaration)
+        {
+            // Vérifier d'abord le namespace filescoped
+            var fileScopedNamespace = classDeclaration.SyntaxTree.GetRoot()
+                .DescendantNodes()
+                .OfType<FileScopedNamespaceDeclarationSyntax>()
+                .FirstOrDefault();
+            if (fileScopedNamespace != null)
+            {
+                return $"{fileScopedNamespace.Name}.{classDeclaration.Identifier.Text}";
+            }
+
+            // Vérifier ensuite le namespace classique
+            var namespaceDeclaration = classDeclaration.Ancestors()
+                .OfType<NamespaceDeclarationSyntax>()
+                .FirstOrDefault();
+            if (namespaceDeclaration != null)
+            {
+                return $"{namespaceDeclaration.Name}.{classDeclaration.Identifier.Text}";
+            }
+
+            return classDeclaration.Identifier.Text;
         }
 
         public static void AnalyzeProjects(IEnumerable<string> projectFiles)
@@ -56,14 +110,14 @@ namespace CodeLineCounter.Services
 
             Parallel.ForEach(classes, classDeclaration =>
             {
-                var className = classDeclaration.Identifier.Text;
+                var className = GetFullTypeName(classDeclaration);
                 var dependencies = ExtractDependencies(classDeclaration);
 
                 foreach (var dependency in dependencies)
                 {
                     var relation = new DependencyRelation
                     {
-                        SourceClass = className,
+                        SourceClass = GetFullTypeName(classDeclaration),
                         TargetClass = dependency,
                         FilePath = filePath,
                         StartLine = classDeclaration.GetLocation().GetLineSpan().StartLinePosition.Line
@@ -91,173 +145,147 @@ namespace CodeLineCounter.Services
         private static IEnumerable<string> ExtractDependencies(ClassDeclarationSyntax classDeclaration)
         {
             var dependencies = new HashSet<string>();
-            var typesToExclude = new HashSet<string>
-            { 
-            // System Object
-            "object",
-            "Object",
-            "System.Object",
-            
-            // Value Types
-            "bool",
-            "Boolean",
-            "System.Boolean",
-            "byte",
-            "Byte",
-            "System.Byte",
-            "sbyte",
-            "SByte",
-            "System.SByte",
-            "char",
-            "Char",
-            "System.Char",
-            "decimal",
-            "Decimal",
-            "System.Decimal",
-            "double",
-            "Double",
-            "System.Double",
-            "float",
-            "Single",
-            "System.Single",
-            "int",
-            "Int32",
-            "System.Int32",
-            "uint",
-            "UInt32",
-            "System.UInt32",
-            "long",
-            "Int64",
-            "System.Int64",
-            "ulong",
-            "UInt64",
-            "System.UInt64",
-            "short",
-            "Int16",
-            "System.Int16",
-            "ushort",
-            "UInt16",
-            "System.UInt16",
-            
-            // String
-            "string",
-            "String",
-            "System.String",
-            
-            // Common Base Types
-            "ValueType",
-            "System.ValueType",
-            "Enum",
-            "System.Enum",
-            "Delegate",
-            "System.Delegate",
-            "MulticastDelegate",
-            "System.MulticastDelegate",
-            
-            // Common System Types
-            "void",
-            "Void",
-            "System.Void",
-            "DateTime",
-            "System.DateTime",
-            "TimeSpan",
-            "System.TimeSpan",
-            "Guid",
-            "System.Guid",
-            
-            // Common Collections
-            "Array",
-            "System.Array",
-            "IEnumerable",
-            "System.Collections.IEnumerable",
-            "IEnumerable<>",
-            "System.Collections.Generic.IEnumerable<>",
-            "ICollection",
-            "System.Collections.ICollection",
-            "ICollection<>",
-            "System.Collections.Generic.ICollection<>",
-            "IList",
-            "System.Collections.IList",
-            "IList<>",
-            "System.Collections.Generic.IList<>",
-            "List<>",
-            "System.Collections.Generic.List<>",
-            "IDictionary",
-            "System.Collections.IDictionary",
-            "IDictionary<,>",
-            "System.Collections.Generic.IDictionary<,>",
-            "Dictionary<,>",
-            "System.Collections.Generic.Dictionary<,>",
-            
-            // Task Types
-            "Task",
-            "System.Threading.Tasks.Task",
-            "Task<>",
-            "System.Threading.Tasks.Task<>",
-            "ValueTask",
-            "System.Threading.Tasks.ValueTask",
-            "ValueTask<>",
-            "System.Threading.Tasks.ValueTask<>"
-        };
-
-        
+            var root = classDeclaration.SyntaxTree.GetRoot();
+            var usings = (root as CompilationUnitSyntax)?.Usings.Select(u => u.Name.ToString()) ?? Enumerable.Empty<string>();
 
             // Analyze inheritance
             if (classDeclaration.BaseList != null)
             {
                 foreach (var baseType in classDeclaration.BaseList.Types)
                 {
-                    var typeName = baseType.Type.ToString();
-                    if (!typesToExclude.Contains(typeName))
+                    var typeName = GetFullTypeNameFromSymbol(baseType.Type.ToString(), usings);
+                    if (_solutionClasses.Contains(typeName))
                     {
                         dependencies.Add(typeName);
                     }
                 }
             }
 
-            // Rest of the method remains the same
-            foreach (var member in classDeclaration.Members)
-            {
-                /*if (member is FieldDeclarationSyntax field)
-                {
-                    foreach (var variable in field.Declaration.Variables)
-                    {
-                        var typeName = field.Declaration.Type.ToString();
-                        if (!typesToExclude.Contains(typeName))
-                        {
-                            dependencies.Add(typeName);
-                        }
-                    }
-                }
-                else */
-                if (member is PropertyDeclarationSyntax property)
-                {
-                    var typeName = property.Type.ToString();
-                    if (!typesToExclude.Contains(typeName))
-                    {
-                        dependencies.Add(typeName);
-                    }
-                }
-                else if (member is MethodDeclarationSyntax method)
-                {
-                    var returnType = method.ReturnType.ToString();
-                    if (!typesToExclude.Contains(returnType))
-                    {
-                        dependencies.Add(returnType);
-                    }
+            // Analyze all nodes in the class
+            var allNodes = classDeclaration.DescendantNodes();
 
-                    foreach (var parameter in method.ParameterList.Parameters)
-                    {
-                        var paramType = parameter.Type.ToString();
-                        if (!typesToExclude.Contains(paramType))
+            foreach (var node in allNodes)
+            {
+                switch (node)
+                {
+                    case FieldDeclarationSyntax field:
+                        var fieldType = GetFullTypeNameFromSymbol(field.Declaration.Type.ToString(), usings);
+                        if (_solutionClasses.Contains(fieldType))
+                        {
+                            dependencies.Add(fieldType);
+                        }
+                        break;
+
+                    case PropertyDeclarationSyntax property:
+                        var propertyType = GetFullTypeNameFromSymbol(property.Type.ToString(), usings);
+                        if (_solutionClasses.Contains(propertyType))
+                        {
+                            dependencies.Add(propertyType);
+                        }
+                        break;
+
+                    case VariableDeclarationSyntax variable:
+                        var varType = GetFullTypeNameFromSymbol(variable.Type.ToString(), usings);
+                        if (_solutionClasses.Contains(varType))
+                        {
+                            dependencies.Add(varType);
+                        }
+                        break;
+
+                    case ObjectCreationExpressionSyntax creation:
+                        var creationType = GetFullTypeNameFromSymbol(creation.Type.ToString(), usings);
+                        if (_solutionClasses.Contains(creationType))
+                        {
+                            dependencies.Add(creationType);
+                        }
+                        break;
+
+                    case InvocationExpressionSyntax invocation:
+                        if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
+                        {
+                            var expressionType = GetFullTypeNameFromSymbol(memberAccess.Expression.ToString(), usings);
+                            if (_solutionClasses.Contains(expressionType))
+                            {
+                                dependencies.Add(expressionType);
+                            }
+                        }
+                        break;
+
+                    case ParameterSyntax parameter:
+                        var paramType = GetFullTypeNameFromSymbol(parameter.Type?.ToString() ?? "", usings);
+                        if (_solutionClasses.Contains(paramType))
                         {
                             dependencies.Add(paramType);
                         }
-                    }
+                        break;
+
+                    case GenericNameSyntax generic:
+                        foreach (var typeArg in generic.TypeArgumentList.Arguments)
+                        {
+                            var genericType = GetFullTypeNameFromSymbol(typeArg.ToString(), usings);
+                            if (_solutionClasses.Contains(genericType))
+                            {
+                                dependencies.Add(genericType);
+                            }
+                        }
+                        break;
+                    case IdentifierNameSyntax identifier:
+                        var identifierType = GetFullTypeNameFromSymbol(identifier.Identifier.Text, usings);
+                        if (_solutionClasses.Contains(identifierType))
+                        {
+                            dependencies.Add(identifierType);
+                        }
+                        break;
+
+                    case MemberAccessExpressionSyntax innerMemberAccess when !(innerMemberAccess.Parent is InvocationExpressionSyntax):
+                        var memberType = GetFullTypeNameFromSymbol(innerMemberAccess.Expression.ToString(), usings);
+                        if (_solutionClasses.Contains(memberType))
+                        {
+                            dependencies.Add(memberType);
+                        }
+                        break;
                 }
             }
 
             return dependencies;
+        }
+
+        private static IEnumerable<string> GetUsings(SyntaxNode node)
+        {
+            var root = node.SyntaxTree.GetRoot();
+            var compilation = root as CompilationUnitSyntax;
+            return compilation?.Usings.Select(u => u.Name.ToString()) ?? Enumerable.Empty<string>();
+        }
+
+        private static string GetFullTypeNameFromSymbol(string typeName, IEnumerable<string> usings)
+        {
+            // Gérer les types génériques
+            if (typeName.Contains("<"))
+            {
+                var baseType = typeName.Substring(0, typeName.IndexOf("<"));
+                var genericPart = typeName.Substring(typeName.IndexOf("<"));
+
+                // Résoudre le type de base
+                var resolvedBaseType = GetFullTypeNameFromSymbol(baseType, usings);
+                return resolvedBaseType + genericPart;
+            }
+
+            // Si le type contient déjà un namespace, on le retourne tel quel
+            if (typeName.Contains("."))
+                return typeName;
+
+            // Chercher dans les using si on trouve le namespace correspondant
+            foreach (var usingStatement in usings)
+            {
+                var fullName = $"{usingStatement}.{typeName}";
+                if (_solutionClasses.Contains(fullName))
+                {
+                    return fullName;
+                }
+            }
+
+            // Vérifier dans le namespace courant
+            return typeName;
         }
 
         public static List<DependencyRelation> GetDependencies()
